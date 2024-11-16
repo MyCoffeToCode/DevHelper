315ecb9a30aa5b5493801756057d8f8c001b3d07
filoroch <pessoal.filiperocha@gmail.com>
2024-11-16 00:03:28 -0300

    Adicionado CommandHandler e PingCommand
    Signed-off-by: filoroch <pessoal.filiperocha@gmail.com>

--- src/main/java/starcode/ICommand.java
+++ src/main/java/starcode/ICommand.java
@@ -2,7 +2,7 @@ package starcode;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-public interface ICommand {
-    void execute(MessageReceivedEvent event);
+public interface ICommand { 
+    void execute(MessageReceivedEvent event); 
     String getCommandName();    
 }
