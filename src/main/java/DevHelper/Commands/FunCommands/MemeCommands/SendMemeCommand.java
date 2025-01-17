24b866c9ab71dd7391ebf50972bee32a89edb795
Andre <dmastergame74@gmail.com>
2025-01-16 22:23:16 -0300

    create feature to add a meme to DB

--- src/main/java/DevHelper/Commands/FunCommands/MemeCommands/SendMemeCommand.java
+++ src/main/java/DevHelper/Commands/FunCommands/MemeCommands/SendMemeCommand.java
@@ -1,7 +1,14 @@
 package DevHelper.Commands.FunCommands.MemeCommands;
 
+import DevHelper.DatabaseManager;
 import DevHelper.ICommand;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Objects;
 
 public class SendMemeCommand implements ICommand {
     @Override
@@ -16,7 +23,27 @@ public class SendMemeCommand implements ICommand {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        // Implementação do comando
-        event.reply("Comando em desenvolvimento!").setEphemeral(true).queue();
+        String url = Objects.requireNonNull(event.getOption("url")).getAsString();
+
+        String query = "INSERT INTO memes VALUES ((SELECT MAX(id) + 1 FROM memes),?,?,?)";
+
+        try(Connection conn = DatabaseManager.connect()) {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, url);
+            stmt.setString(2, "Programação");
+            stmt.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+
+            int result = stmt.executeUpdate();
+
+            if (result != 0) {
+                event.reply("Aeeê! Seu meme foi adicionado com sucesso! 🎉").setEphemeral(true).queue();
+            } else {
+                event.reply("Erro ao adicionar seu meme! 😢 Por favor, tenta outra vez.").setEphemeral(true).queue();
+            }
+        } catch (SQLException e) {
+            event.reply("Erro ao adicionar o meme! 😢 Detalhes: " + e.getMessage()).setEphemeral(true).queue();
+            throw new RuntimeException(e);
+        }
+
     }
 }
